c9399eaee1eca8da2a2e984b5c85bdd8548d0e0c
t-tani <31282248+t-tani@users.noreply.github.com>
2020-09-01 22:10:06 +0900

    test

--- test.py
+++ test.py
@@ -2,8 +2,12 @@ try:
     from volatility.framework import interfaces, renderers
     from volatility.framework.configuration import requirements
     from volatility.plugins import yarascan
+    print("interfaces, renderers, requirements -> ok!")
+except ImportError:
+    print("Failed 0x01")
+try:
     from volatility.plugins.windows import pslist
     from volatility.plugins.windows import vaddump
     print("Success to import volatility modules")
 except ImportError:
-    raise ImportError("volatility modules not found.")
+    print("Failed 0x02")
