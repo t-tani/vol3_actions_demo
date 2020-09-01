359b5804d0599b863f0eaa2f722820685bd29d19
t-tani <31282248+t-tani@users.noreply.github.com>
2020-09-01 22:20:09 +0900

    test

---
+++ add_pythonpath.py
@@ -0,0 +1,8 @@
+import os
+import sys
+path=[path for path in sys.path if "site-packages" in path][0]
+vollib=[path for path in os.listdir(path) if "volatility" in path][0]
+sys.path.append(path+"/"+vollib)
+print(os.pathdir(path+"/"+vollib))
+cmd = "::set-env name=PYTHONPATH:"+"":".join(sys.path)
+print(os.environ)
